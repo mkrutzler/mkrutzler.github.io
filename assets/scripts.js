9cb91c0b6950dbc07f67bdf3abaaf4a3d57ae19d
Mark Krutzler <mark.krutzler@icloud.com>
2025-01-14 19:28:57 +0100

    proper finished product (except blogs)

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,23 +1,37 @@
 function openTab(evt, tabName) {
-    // Declare all variables
     var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
-  } 
+}
 
   // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.img-container img');
+    const overlays = document.querySelectorAll('.img-overlay');
+
+    function toggleImageSize(event) {
+        const imgContainer = event.target.closest('.img-container');
+        const img = imgContainer.querySelector('img');
+        const overlay = imgContainer.querySelector('.img-overlay');
+        img.classList.toggle('expanded');
+        overlay.classList.toggle('expanded');
+    }
+
+    images.forEach(image => {
+        image.addEventListener('click', toggleImageSize);
+    });
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener('click', toggleImageSize);
+    });
+});
